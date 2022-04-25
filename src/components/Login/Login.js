9b90d8bcf36ec790a70a95d7e13e6fb09cf0373e
Leonardo Canteros <leo.sebas.065@gmail.com>
2022-04-25 14:58:27 -0300

    Avanse de la Register Page. Terminado Login y Success Page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,20 @@
 import { LoginForm } from "./LoginForm";
+import { RegisterForm } from "../Register/Register";
 import "./Login.css"
+import { useState } from "react";
 export function Login({userChanged}) {
+  const [register, setRegister] = useState(false)
+
+  function handleRegister() {
+    setRegister(true)
+  }
+
   return (
     <main className="LoginPage">
         <h2 className ="LoginTitle">Stax Food Admin Login</h2>
-        <LoginForm userChanged={userChanged} />
+        {register 
+          ? <RegisterForm />
+          : <LoginForm userChanged={userChanged} handleRegister={ handleRegister } />}
     </main>
   );
 }
